268098bf4ac6559ac1f67d9595351eaa4a1d559a
Your Nam <you@example.com>
2021-12-11 01:58:35 -0600

    Part 7

--- coursenotes.md
+++ coursenotes.md
@@ -1,2 +1,7 @@
 add app to installed app list for django to get the templates
 template inheritence
+
+user built in managment system
+    user.is_authenticated
+    @login_required decorator
+    add settings to configure built in managment system
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,4 @@
-"""django_project URL Configuration
+'''django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -12,14 +12,18 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('register/', user_views.register , name="user-register")
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 
 ]
